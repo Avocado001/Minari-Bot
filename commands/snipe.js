a8295035489a3329ae1c07eff37a8b672fd4f897
root <root@ip-172-31-38-145.ap-southeast-1.compute.internal>
2020-09-07 16:56:24 +0000

    changes in snipe

--- commands/snipe.js
+++ commands/snipe.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 
 module.exports.run = async(bot, message, args) =>{
-    if(message.member.hasPermission('MANAGE_MESSAGES')) {
+    if(message.member.hasPermission('MANAGE_ROLES')) {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
 
