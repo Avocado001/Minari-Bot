40148fe8e4e71808bdea2464c4322c0ebb47549f
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-02 22:36:38 +0800

    Update dahyun.js

--- commands/dahyun.js
+++ commands/dahyun.js
@@ -1,20 +1,57 @@
+const https = require('https');
 const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const url = 'https://www.reddit.com/r/dahyun/hot/.json?limit=100'
 
-module.exports.run = async (bot, message, args) => {
-    const subReddits = ["dahyun"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    const img = await randomPuppy(random);
+module.exports.run = async (bot, message, args) => {  
 
-    const embed = new Discord.MessageEmbed()
-    .setImage(img)
-    .setTitle(`From /r/${random}`)
-    .setURL(`http://reddit.com/r/${random}`)
-    .setColor('#FFFFFF')
-    .setFooter('Dont forget to t!rep the admins')
+        https.get(url, (result) => {
+            var body = ''
+            result.on('data', (chunk) => {
+                body += chunk
+            })
 
-    message.channel.send(embed);
+            result.on('end', () => {
+                var response = JSON.parse(body)
+                var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
 
+                if (index.post_hint !== 'image') {
+
+                    var text = index.selftext
+                    const textembed = new Discord.MessageEmbed()
+                        .setTitle(subRedditName)
+                        .setColor(FFFFFE)
+                        .setDescription(`[${title}](${link})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+
+                    message.channel.send(textembed)
+                }
+
+                var image = index.preview.images[0].source.url.replace('&amp;', '&')
+                var title = index.title
+                var link = 'https://reddit.com' + index.permalink
+                var subRedditName = index.subreddit_name_prefixed
+
+                if (index.post_hint !== 'image') {
+                    const textembed = new Discord.RichEmbed()
+                        .setTitle(subRedditName)
+                        .setColor(FFFFFE)
+                        .setDescription(`[${title}](${link})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+
+                    message.channel.send(textembed)
+                }
+                console.log(image);
+                const imageembed = new Discord.MessageEmbed()
+                    .setTitle(subRedditName)
+                    .setImage(image)
+                    .setColor(FFFFFE)
+                    .setDescription(`[${title}](${link})`)
+                    .setURL(`https://reddit.com/${subRedditName}`)
+                message.channel.send(imageembed)
+            }).on('error', function (e) {
+                console.log('Got an error: ', e)
+            })
+        })
 }
 
 module.exports.config = {
