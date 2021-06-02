b295c4375f75e27aded663df77e61b6a5c83bd37
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-03 01:57:47 +0800

    Update sana.js

--- commands/sana.js
+++ commands/sana.js
@@ -1,21 +1,31 @@
 const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const got = require('got');
 
-module.exports.run = async (bot, message, args) => {    
-    const subReddits = ["NoSanaNoLife"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    const img = await randomPuppy(random);
+module.exports.run = async (bot, message, args) => {
+	const embed = new Discord.MessageEmbed();
+	got('https://www.reddit.com/r/NoSanaNoLife/random/.json')
+		.then(response => {
+			const [list] = JSON.parse(response.body);
+			const [post] = list.data.children;
 
-    const embed = new Discord.MessageEmbed()
-    .setImage(img)
-    .setTitle(`From /r/${random}`)
-    .setURL(`http://reddit.com/r/${random}`)
-    .setColor('#9FA8DA')
-    .setFooter('Dont forget to t!rep the admins')
+			const permalink = post.data.permalink;
+			const memeUrl = `https://reddit.com${permalink}`;
+			const memeImage = post.data.url;
+			const memeTitle = post.data.title;
+			const memeUpvotes = post.data.ups;
+			const memeNumComments = post.data.num_comments;
 
-    message.channel.send(embed);
+			embed.setTitle(`${memeTitle}`);
+			embed.setURL(`${memeUrl}`);
+			embed.setColor('#9FA8DA');
+			embed.setImage(memeImage);
+			embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
 
-}
+			message.channel.send(embed);
+		})
+		.catch(console.error);
+};
+
 
 module.exports.config = {
     name: "sana",
