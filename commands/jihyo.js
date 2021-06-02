9ad6cdff1ccd7f43de15a3aa20735cfa446a7cda
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-03 01:52:24 +0800

    Update jihyo.js

--- commands/jihyo.js
+++ commands/jihyo.js
@@ -1,20 +1,30 @@
 const Discord = require('discord.js');
-const randomPuppy = require('random-puppy');
+const got = require('got');
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["ParkJihyo"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    const img = await randomPuppy(random);
+	const embed = new Discord.MessageEmbed();
+	got('https://www.reddit.com/r/ParkJihyo/random/.json')
+		.then(response => {
+			const [list] = JSON.parse(response.body);
+			const [post] = list.data.children;
 
-    const embed = new Discord.MessageEmbed()
-    .setImage(img)
-    .setTitle(`From /r/${random}`)
-    .setURL(`http://reddit.com/r/${random}`)
-    .setColor('#FFB74D')
-    .setFooter('Dont forget to t!rep the admins')
-    message.channel.send(embed);
+			const permalink = post.data.permalink;
+			const memeUrl = `https://reddit.com${permalink}`;
+			const memeImage = post.data.url;
+			const memeTitle = post.data.title;
+			const memeUpvotes = post.data.ups;
+			const memeNumComments = post.data.num_comments;
 
-}
+			embed.setTitle(`${memeTitle}`);
+			embed.setURL(`${memeUrl}`);
+			embed.setColor('#FFB74D');
+			embed.setImage(memeImage);
+			embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+
+			message.channel.send(embed);
+		})
+		.catch(console.error);
+};
 
 module.exports.config = {
     name: "jihyo",
