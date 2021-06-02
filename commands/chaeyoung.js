eb237cf478353fef3300b4a117870f0899d431ec
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-02 17:24:24 +0800

    Update chaeyoung.js

--- commands/chaeyoung.js
+++ commands/chaeyoung.js
@@ -2,33 +2,25 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-            let content = JSON.parse(response.body);
-            let permalink = content[0].data.children[0].data.permalink;
-            let memeUrl = `https://reddit.com${permalink}`;
-            let memeImage = content[0].data.children[0].data.url;
-            let memeTitle = content[0].data.children[0].data.title;
-            let memeUpvotes = content[0].data.children[0].data.ups;
-            let memeDownvotes = content[0].data.children[0].data.downs;
-            let memeNumComments = content[0].data.children[0].data.num_comments;
-    
-    got('https://www.reddit.com/r/memes/random/.json').then(response => {
-            const embed = new Discord.MessageEmbed()
-            .setTitle(`${memeTitle}`)
-            .setURL(`${memeUrl}`)
-            .setImage(memeImage)
-            .setColor('RANDOM')
-            .setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
-            
-            message.channel.send(embed);
-        })
-    }
+    const subReddits = ["chaeyoung"]
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    const img = await randomPuppy(random);
+
+    const embed = new Discord.MessageEmbed()
+    .setImage(img)
+    .setTitle(`From /r/${random}`)
+    .setURL(`http://reddit.com/r/${random}`)
+    .setColor('#FF1744')
+    .setFooter('Dont forget to t!rep the admins')
+
+    message.channel.send(embed);
 
 }
 
 module.exports.config = {
-    name: "chaeyoung",
-    description: "Send an image of Son Chaeyoung",
-    usage: "=chaeyoung",
+    name: "dahyun",
+    description: "Send an image of Kim Dahyun",
+    usage: "=dahyun",
     accessableby: "Members",
-    aliases: ["chae", "cub", "tiger", "smol", "babybeast", "strawberry", "seyoung"]
+    aliases: ["dubu", "tofu", "dinosaur", "bibimbap", "bonjuruu", "waowexpensive", "eagle"]
 }
