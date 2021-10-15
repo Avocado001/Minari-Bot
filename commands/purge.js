2598143dd840719c7f1931750cac3235471ff734
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-10-15 17:07:41 +0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,11 +2,17 @@ module.exports.run = async (bot, message) => {
     const messageArray = message.content.split(' ');
 	const args = messageArray.slice(1);
 
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
+    if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+	    return message.channel.send('Lack of Perms!')
+	    .then(msg => msg.delete({timeout:5000}));
+    }
     
     let deleteAmount;
 
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) { 
+	return message.reply('Please put a number only!') 
+	.then(msg => msg.delete({timeout:5000}))
+    }
 
     if (parseInt(args[0]) > 100) {
         return message.reply('You can only delete 100 messages at a time!')
